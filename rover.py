8e8effd2a3e35ebc77de46100afd3401a68b57b1
Wincenty Skwarek <wincentyskwarek@op.pl>
2024-06-12 13:45:59 +0200

    Opis wprowadzonych zmian

--- rover.py
+++ rover.py
@@ -120,11 +120,17 @@ class Rover():
             self.wheel3p.ChangeDutyCycle(0)
             self.wheel26p.ChangeDutyCycle(0)
             self.wheel4p.ChangeDutyCycle(0)
-        
+
+            self.servo1.ChangeDutyCycle(90)
+            self.servo2.ChangeDutyCycle(90)
+            self.servo5.ChangeDutyCycle(90)
+            self.servo6.ChangeDutyCycle(90)
+ '''       
         self.servo1.ChangeDutyCycle(2 + (angle/ 18))
         self.servo2.ChangeDutyCycle(2 + (angle/ 18))
-        self.servo5.ChangeDutyCycle(2 + ((angle+240)/ 18))
+        self.servo5.ChangeDutyCycle(2 + ((angle+60)/ 18))
         self.servo6.ChangeDutyCycle(2 + ((angle+85)/ 18))
+'''
         # Wheel 1 and wheel 5
         
         #angle1=int(angle1*Rover.servoAngle)//100
