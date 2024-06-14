32dd2e9f41069b1543784217a5d3f41116c8b59e
Wincenty Skwarek <wincentyskwarek@op.pl>
2024-06-14 14:50:52 +0200

    Opis wprowadzonych zmian

--- program.py
+++ program.py
@@ -48,8 +48,8 @@ while True:
     # Odczyt z odbiornika i zapis do listy kanałów
     res = ibus_in.read()
     if (res[0] == 1):
-        kat=IBus.normalize(res[1])
-        predkosc=IBus.normalize(res[3])
+        kat=IBus.normalize(res[3])
+        predkosc=IBus.normalize(res[1])
         lazik.go(kat,predkosc)
         print(kat)
         
