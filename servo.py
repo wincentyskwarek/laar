58f89c854488036409e672766dd9e98ca29fc468
Wincenty Skwarek <wincentyskwarek@op.pl>
2024-06-14 09:25:45 +0200

    Opis wprowadzonych zmian

--- servo.py
+++ servo.py
@@ -1,31 +1,26 @@
 import RPi.GPIO as GPIO
-import time
-pin=27
-# Ustawienia GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin, GPIO.OUT)  # GPIO17 jako wyjście
+import time 
+# use P1 header pin numbering convention
+GPIO.setmode(GPIO.BOARD)
 
-# Ustawienie PWM na pinie GPIO17
-pwm = GPIO.PWM(pin, 50)  # 50Hz częstotliwość
-pwm.start(0)  # Rozpocznij PWM z wypełnieniem 0
-
-# Funkcja do ustawiania kąta serwa
-def set_angle(angle):
-    duty = 2 + (angle / 18)  # Przekształcenie kąta na cykl pracy PWM
-    GPIO.output(pin, True)
-    pwm.ChangeDutyCycle(duty)
-    time.sleep(1)
-    GPIO.output(pin, False)
-    pwm.ChangeDutyCycle(0)
+GPIO.setup(22,GPIO.OUT)
+pwm = GPIO.PWM(22,50)
+pwm.start(2)
+time.sleep(1)
 
 try:
-    while True:
-        set_angle(0)  # Ustaw serwo na 0 stopni
-        time.sleep(2)
-        set_angle(20)  # Ustaw serwo na 20 stopni
-        time.sleep(2)
-        set_angle(40)  # Ustaw serwo na 40 stopni
-        time.sleep(2)
+dane = -1
+
+while (dane != "0"):			
+	dane = raw_input("Wprowadz cyfre:");		
+	pwm.ChangeDutyCycle(float(dane))	
+	time.sleep(.03)
+
+
 except KeyboardInterrupt:
-    pwm.stop()
-    GPIO.cleanup()
+pwm.stop()
+GPIO.cleanup()
+
+pwm.stop()
+GPIO.cleanup()
+print "Konic"
