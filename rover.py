19c36a3933c319ec2d319c1f6d7ef661fce6b120
Wincenty Skwarek <wincentyskwarek@op.pl>
2024-06-17 14:54:05 +0200

    Opis wprowadzonych zmian

--- rover.py
+++ rover.py
@@ -104,10 +104,10 @@ class Rover():
                 self.wheel4p.ChangeDutyCycle(0) # PSrodekP wylacz
                 self.wheel4t.ChangeDutyCycle(speed) # PSrodekT wylacz
             elif angle == 100: 
-                self.wheel3p.ChangeDutyCycle(speed) # PSrodekP wlacz
-                self.wheel3t.ChangeDutyCycle(0) # PSrodekT wylacz             
-                self.wheel4t.ChangeDutyCycle(speed) # LSrodekT wlacz
-                self.wheel4p.ChangeDutyCycle(0) # LSrodekP wylacz
+                self.wheel3p.ChangeDutyCycle(speed) # LSrodekP wlacz
+                self.wheel3t.ChangeDutyCycle(0) # LSrodekT wylacz
+                self.wheel4t.ChangeDutyCycle(0) # PSrodekT wylacz
+                self.wheel4p.ChangeDutyCycle(speed) # PSrodekP wlacz
             else:
                 pass
 
