a822ec7777bdcab05ad78bb289164c437567c731
Wincenty Skwarek <wincentyskwarek@op.pl>
2024-06-19 12:52:18 +0200

    Opis wprowadzonych zmian

--- program.py
+++ program.py
@@ -51,7 +51,7 @@ while True:
         kat=IBus.normalize(res[4])
         predkosc=IBus.normalize(res[2])
         lazik.go(kat, predkosc)
-        print(f"kat: {kat}, predkosc: {predkosc},  {gps}")
+        print(f"kat: {kat}, predkosc: {predkosc}")
         
         # zmiana kierunku srodkowego kola wzgledem katu obrotu:
 
