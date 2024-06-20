5e70c8b857bb9cec140d2fd71a68b73bb6ce1963
Wincenty Skwarek <wincentyskwarek@op.pl>
2024-06-20 11:08:54 +0200

    Opis wprowadzonych zmian

--- rover.py
+++ rover.py
@@ -95,6 +95,29 @@ class Rover():
             speed26=speed
             r=1000
         
+        if speed>0:
+            self.wheel15p.ChangeDutyCycle(speed15)
+            self.wheel3p.ChangeDutyCycle(speed3)
+            self.wheel26p.ChangeDutyCycle(speed26)
+            self.wheel4p.ChangeDutyCycle(speed4)
+            self.wheel15t.ChangeDutyCycle(0)
+            self.wheel3t.ChangeDutyCycle(0)
+            self.wheel26t.ChangeDutyCycle(0)
+            self.wheel4t.ChangeDutyCycle(0)                       
+        else:
+            speed15=-speed15
+            speed26=-speed26
+            speed3=-speed3
+            speed4=-speed4
+            self.wheel15t.ChangeDutyCycle(speed15)
+            self.wheel3t.ChangeDutyCycle(speed3)
+            self.wheel26t.ChangeDutyCycle(speed26)
+            self.wheel4t.ChangeDutyCycle(speed4)
+            self.wheel15p.ChangeDutyCycle(0)
+            self.wheel3p.ChangeDutyCycle(0)
+            self.wheel26p.ChangeDutyCycle(0)
+            self.wheel4p.ChangeDutyCycle(0)
+
         self.servo1.ChangeDutyCycle(7.5-alpha1)
         self.servo2.ChangeDutyCycle(7.2-alpha2)
         
