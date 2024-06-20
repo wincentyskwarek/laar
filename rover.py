4d347ab3c709d6a7af922d9fc1535cf1df2147df
Wincenty Skwarek <wincentyskwarek@op.pl>
2024-06-20 11:03:48 +0200

    poprawka

--- rover.py
+++ rover.py
@@ -65,7 +65,7 @@ class Rover():
     def go(self, angle, speed):
         if speed<5 and speed>-5:
             speed=0
-        angle*=0.8
+        angle*=0.9
         #self.wheel26p.ChangeDutyCycle(100)
         b=1
         # Odległość od środka łazika do środka koła szerokość
@@ -73,7 +73,7 @@ class Rover():
         # Odległość od środka łazika do środka koła długość
         y=0.64
         if angle!=0:
-            r=b/(2*tan(radians(angle)))
+            r=b/(4*tan(radians(angle)))
             alpha1=degrees(atan(y/(r-x)))
             alpha2=degrees(atan(y/(r+x)))
             r1=sqrt((r-x)**2+y**2)
