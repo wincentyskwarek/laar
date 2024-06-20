2f89fcf848f9f89a75f2518fc170558f9b86b431
Wincenty Skwarek <wincentyskwarek@op.pl>
2024-06-20 13:53:39 +0200

    poprawka

--- rover.py
+++ rover.py
@@ -72,6 +72,10 @@ class Rover():
             speed15=speed
             speed26=speed
             speed3=speed*(100+angle*14.6)/100
+            if angle*14.6>-80:
+                speed4=speed*(100-angle*14.6)/100
+            else:
+                speed4=-speed*5*(angle*14.6+80)/100
             speed4=speed
 
         elif angle>0:
@@ -80,11 +84,6 @@ class Rover():
             speed15=speed
             speed26=speed
             speed3=speed
-
-            if angle*14.6<80:
-                speed4=speed*(100-angle*14.6)/100
-            else:
-                speed4=-speed*5*(angle*14.6-80)/100
         else:
             alpha1=angle
             alpha2=angle
